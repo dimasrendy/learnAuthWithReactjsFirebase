d6e6e70c15c72f66619b912418ac21e929b3e49e
dimasrendy <dimasrendy00@gmail.com>
2022-04-11 23:00:17 +0700

    Final Version. Add forget password, login, signup function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import { Container } from "react-bootstrap";
-import Signup from "./components/Signup";
 import { AuthProvider } from "./contexts/AuthContext";
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Components
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import ForgotPassword from "./components/ForgotPassword";
+import UpdateProfile from "./components/UpdateProfile";
 
 function App() {
+
   return (
-    <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-        <div className="w-100" style={{ maxWidth: '400px' }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <BrowserRouter>
+        <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+          <div className="w-100" style={{ maxWidth: '400px' }}>
+            <AuthProvider>
+              <Routes>
+                <Route path="/" element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                } />
+                <Route path='/update-profile' element={
+                  <PrivateRoute>
+                    <UpdateProfile />
+                  </PrivateRoute>
+                } />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+              </Routes>
+            </AuthProvider>
+          </div>
+        </Container>
+    </BrowserRouter>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,29 @@
 import { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
-import { Link } from 'react-router-dom';
 
-const Signup = () => {
+const Login = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const { signup } = useAuth();
+  const { login } = useAuth();
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
+
+  //calling useNavigate hook
+  let navigate = useNavigate();
+
 
   async function handleSubmit(e) {
     e.preventDefault();
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError('Password do not match!');
-    }
-
     try {
       setError('');
       setLoading(true);
-      await signup(emailRef.current.value, passwordRef.current.value);
+      await login(emailRef.current.value, passwordRef.current.value);
+      navigate('/');
     } catch (error) {
-      setError('Failed to create an account')
+      setError('Faild to sign in')
     }
     setLoading(false);
   }
@@ -32,7 +32,7 @@ const Signup = () => {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+          <h2 className="text-center mb-4">Login</h2>
           {error && <Alert variant='danger'>{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id='email'>
@@ -43,19 +43,18 @@ const Signup = () => {
               <Form.Label>Password</Form.Label>
               <Form.Control type='password' ref={passwordRef} required />
             </Form.Group>
-            <Form.Group id='password-confirm'>
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type='password' ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button disable='{loading}' type='submit' className='w-100 mt-3 text-center'>Sign Up</Button>
+            <Button disable='{loading}' type='submit' className='w-100 mt-3'>Login</Button>
           </Form>
+          <div className='w-100 mt-3 text-center'>
+            <Link to='/forgot-password'>Forgot Password</Link>
+          </div>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Login</Link>
+        Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </>
   );
 }
 
-export default Signup;
+export default Login;
